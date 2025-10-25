7c5891e02325080a6ce47175c9306caae103411f
anusreesjith <anusreesjith@gmail.com>
2025-10-25 20:38:15 +0530

    redesigning movie suggestion section

--- src/components/GptMovieSuggestions.js
+++ src/components/GptMovieSuggestions.js
@@ -9,20 +9,33 @@ const GptMovieSuggestions = () => {
   if (!movieResults) return;
 
   return (
-    <div className="gpt-movie-container">
-      {movieResults.map((perMovieResults) => {
-        return perMovieResults.map((result) => {
-          if (result.poster_path && result.popularity > 1) {
-            return (
-              <MovieCard
-                key={result.id}
-                visiblity={true}
-                poster={result.poster_path}
-                calledFromGptSearch="GptMovieSuggestions"
-              />
-            );
-          }
-        });
+    <div className="gpt-whole-movie-container">
+      {movieResults.map((perMovieResults, index) => {
+        return (
+          <>
+            {perMovieResults.length > 0 && (
+              <div className="my-4">
+                <h1 className="text-white font-bold text-xl py-2">
+                  {movieNames[index]}
+                </h1>
+                <div className="gpt-movie-container">
+                  {perMovieResults.map((result) => {
+                    if (result.poster_path && result.popularity > 1) {
+                      return (
+                        <MovieCard
+                          key={result.id}
+                          visiblity={true}
+                          poster={result.poster_path}
+                          calledFromGptSearch="GptMovieSuggestions"
+                        />
+                      );
+                    }
+                  })}
+                </div>
+              </div>
+            )}
+          </>
+        );
       })}
     </div>
   );
