54a273bfc6befe6f331f99685c9851004f584b87
anusreesjith <anusreesjith@gmail.com>
2025-12-31 15:27:51 +0530

    vercel setups

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import "dotenv/config";
+import { MOVIE_API_OPTIONS } from "./constants.js";
 
 const app = express();
 
@@ -6,7 +8,27 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
-const port = process.env.PORT || 3001;
-app.listen(port, () => {
-  console.log(`Server started at port ${port}`);
+app.get("/api/now_playing", async (req, res) => {
+  try {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?page=1",
+      MOVIE_API_OPTIONS
+    );
+
+    if (!data.ok) throw new Error(`HTTP error, status: ${data.status}`);
+
+    const json = await data.json();
+    res.json(json);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: `Failed to fetch nowPlayingMovies : ${error.message}` });
+  }
 });
+
+// const port = process.env.PORT || 3001;
+// app.listen(port, () => {
+//   console.log(`Server started at port ${port}`);
+// });
+
+export default app;
